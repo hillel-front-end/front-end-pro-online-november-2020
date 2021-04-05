b286efbbe68b4c93df1ae10c2716278f8497d102
Aleksandr Sayko <aleksandr.sayko@tradetech-il.com>
2021-04-05 21:45:53 +0300

    Lection 33

--- lections/lection_33_react_practice/l-33/src/services/weatherService.js
+++ lections/lection_33_react_practice/l-33/src/services/weatherService.js
@@ -1,4 +1,4 @@
-const API_KEY = 'fe147517cb42a8032190ecae01b8741c'; // Not commit to repo
+const API_KEY = '***'; // Not commit to repo
 
 export const loadWeatherByCityName = cityName => {
     return fetch(`http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric&q=${cityName}`)
